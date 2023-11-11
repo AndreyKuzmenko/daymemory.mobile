c6c8266e4428cdce61e88a4159ef816e837327fc
Andrii Kuzmenko <a.kuzmenko2007@gmail.com>
2023-11-11 17:17:20 +0100

    statusbar color fixed on splash screen

--- lib/widget/common/splash/splash_widget.dart
+++ lib/widget/common/splash/splash_widget.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class SplashWidget extends StatelessWidget {
   const SplashWidget({super.key});
 
   @override
-  Widget build(BuildContext context) => const Center(
-        child: CircularProgressIndicator(),
-      );
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme.secondary;
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarIconBrightness: Theme.of(context).brightness == Brightness.dark ? Brightness.light : Brightness.dark,
+        statusBarBrightness: Theme.of(context).brightness == Brightness.dark ? Brightness.dark : Brightness.light,
+      ),
+      child: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(color: color),
+        ),
+      ),
+    );
+  }
 }
